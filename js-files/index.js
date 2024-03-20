7e28131b0aa37bbecc91094414c60ac2730ce48c
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-03-20 05:51:34 -0400

    Modify and Rename Modify index.js Rename pp.js to Header.js

--- js-files/index.js
+++ js-files/index.js
@@ -1,4 +1,4 @@
-import { Header } from "./components/app.js";
+import { Header } from "./components/Header.js";
 // import { landing } from "./landing.js";
 
 customElements.define("main-header", Header);
